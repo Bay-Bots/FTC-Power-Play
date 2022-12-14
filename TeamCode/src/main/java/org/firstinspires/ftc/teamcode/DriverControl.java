69d2abd19274ef1e27cbe9650a48ebaa92d52b0f
QuinnRainey314 <90530493+QuinnRainey314@users.noreply.github.com>
2022-12-14 13:41:11 -0500

    Backup from OBJ
    Added linear slide, claw, new updates, etc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -1,18 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.teamcode.Base.RobotStructure;
+import org.firstinspires.ftc.teamcode.base.RobotStructure;
 
 @TeleOp(name="DriverControl")
 public class DriverControl extends RobotStructure {
+    
+
     public void loop() {
-    double armSpeed = 0.75 * gamepad2.left_stick_y;
-    setArmSpeed(-armSpeed);
+
     initDriver();
+    
 
     while (gamepad1.dpad_down) {
     setDriverMotorPower(0.25,0.25,0.25, 0.25);
@@ -35,15 +38,45 @@ while (gamepad1.dpad_right){
 
      if (!gamepad1.dpad_right){
         translateRight(0);
-    }
+    } 
 }
-
 while (gamepad1.dpad_left){
     translateLeft(0.25);
 
       if (!gamepad1.dpad_left){
         translateLeft(0);
+    } 
+}  
+while (gamepad2.dpad_up){
+    linearSlide.setPower(-0.5);
+
+     if (!gamepad2.dpad_up){
+    linearSlide.setPower(0);
     }
 }
+while (gamepad2.dpad_down){
+    linearSlide.setPower(0.5);
+     if (!gamepad2.dpad_down){
+    linearSlide.setPower(0);
+    }
+}
+
+
+//        
+        if(gamepad2.a) {
+            //setClawPos(1, 1);
+        servoClaw1.setPosition(1);
+        servoClaw2.setPosition(1);
+        }
+
+//        
+        if(gamepad2.b) {
+       // setClawPos(0.5, 0.5);
+        servoClaw1.setPosition(0.5);
+        servoClaw2.setPosition(0.5);
+        }
+
+
+
 }
 }

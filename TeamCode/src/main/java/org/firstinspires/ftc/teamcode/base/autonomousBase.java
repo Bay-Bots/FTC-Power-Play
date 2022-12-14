e6f816551ac75e82509054da8c342a2a922feec2
QuinnRainey314 <90530493+QuinnRainey314@users.noreply.github.com>
2022-12-14 13:54:22 -0500

    Update and rename autonomousBase to autonomousBase.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/autonomousBase.java
@@ -0,0 +1,179 @@
+package org.firstinspires.ftc.teamcode.Base;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class AutoRobotStruct extends LinearOpMode {
+    private DcMotor motorFrontRight;
+    private DcMotor motorFrontLeft;
+    private DcMotor motorBackRight;
+    private DcMotor motorBackLeft;
+    private Servo servoClaw1;
+    private Servo servoClaw2;
+    private DcMotor linearSlide;
+    // accessed in RedDuck.java directly without get/set methods therefore not private vars
+    public ColorSensor colorSensor;
+
+    @Override
+    public void runOpMode() throws InterruptedException { }
+
+    public void initRunner() throws InternalError {
+        motorFrontRight = hardwareMap.get(DcMotor.class, "motor front right");
+        motorFrontLeft = hardwareMap.get(DcMotor.class, "motor front left");
+        motorBackLeft = hardwareMap.get(DcMotor.class, "motor back left");
+        motorBackRight = hardwareMap.get(DcMotor.class, "motor back right");
+        linearSlide = hardwareMap.get(DcMotor.class, "LinearSlide");
+        servoClaw1 = hardwareMap.get(Servo.class, "servo claw1");
+        servoClaw2 = hardwareMap.get(Servo.class, "servo claw2");
+        colorSensor = hardwareMap.get(ColorSensor.class, "color sensor");
+
+        // reverse direction for this drive train
+        motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public double getDistanceFront() {
+        return distanceFront.getDistance(DistanceUnit.INCH);
+    }
+
+    public double getDistanceBack() {
+        return distanceBack.getDistance(DistanceUnit.INCH);
+    }
+
+    public void setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower) {
+        motorFrontRight.setPower(FRightPower);
+        motorFrontLeft.setPower(FLeftPower);
+        motorBackLeft.setPower(BLeftPower);
+        motorBackRight.setPower(BRightPower);
+    }
+
+    public void setDriverMotorPower(double FRightPower, double FLeftPower, double BRightPower, double BLeftPower, int s) {
+        motorFrontRight.setPower(FRightPower);
+        motorFrontLeft.setPower(FLeftPower);
+        motorBackLeft.setPower(BLeftPower);
+        motorBackRight.setPower(BRightPower);
+        sleep(s);
+
+        motorFrontRight.setPower(0);
+        motorFrontLeft.setPower(0);
+        motorBackLeft.setPower(0);
+        motorBackRight.setPower(0);
+        sleep(100);
+    }
+
+    public void setDistanceAndMoveForward(double _front_distance){
+        double distFront = getDistanceFront();
+        telemetry.addData("Dist front ", distFront);
+
+        while (distFront > _front_distance) {
+            // telemetry.addData("Dist front ", distFront);
+            // move forward
+            setDriverMotorPower(0.25,0.25,0.25,0.25);
+            distFront = getDistanceFront();
+        }
+
+        setDriverMotorPower(0.0,0.0,0.0,0.0, 10);
+    }
+
+    public void setDistanceAndMoveBackward(double back_distance){
+        double distBack = getDistanceBack();
+        telemetry.addData("Dist back ", distBack);
+
+        while (distBack > back_distance) {
+            // move back
+            setDriverMotorPower(-0.25,-0.25,-0.25,-0.25);
+            distBack = getDistanceBack();
+        }
+
+        setDriverMotorPower(0.0,0.0,0.0,0.0, 10);
+    }
+
+    public void setDistanceAndMoveForwardFromBackSensor(double wallDistance){
+        double distBack = getDistanceBack();
+        telemetry.addData("Dist Back ", distBack);
+        telemetry.update();
+
+        while (distBack < wallDistance) {
+            // telemetry.addData("Dist front ", distFront);
+            // move forward
+            setDriverMotorPower(0.2,0.2,0.2,0.2);
+            distBack = getDistanceBack();
+        }
+
+        setDriverMotorPower(0.0,0.0,0.0,0.0, 10);
+    }
+
+    public void setDuckDropperSpeed(double speed, int s){
+        motorDuckDropper.setPower(-speed);
+        sleep(s);
+
+        motorDuckDropper.setPower(0);
+        sleep(10);
+    }
+
+    public void STOP_AND_RESET() {
+        motorArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorArmDuo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void SET_TARGET_POWER_RUN(int position, double power) {
+        motorArmDuo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorArmDuo.setTargetPosition(position);
+        motorArm.setTargetPosition(-position);
+
+        motorArmDuo.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorArmDuo.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorArm.setPower(-power);
+        motorArmDuo.setPower(power);
+
+        while (motorArmDuo.isBusy()) {
+            telemetry.addData("left en", motorArm.getCurrentPosition());
+            telemetry.addData("right en", motorArmDuo.getCurrentPosition());
+            telemetry.update();
+
+            idle();
+        }
+    }
+
+    public void SET_ARM_POWER_ZERO(){
+        motorArmDuo.setPower(0);
+        motorArm.setPower(0);
+    }
+
+    public void setClawPos(double pos1, double pos2) {
+        servoClaw1.setPosition(pos1);
+        servoClaw2.setPosition(pos2);
+    }
+
+    public void moveIntake(double intakeSpeed) {
+        motorIntake.setPower(intakeSpeed);
+    }
+
+    public void releaseHoldGate() {
+        servoHold.setPosition(0);
+    }
+
+    public void pushIntake() {
+        servoPush.setPosition(1);
+        sleep(100);
+        servoPush.setPosition(0);
+        sleep(100);
+        servoPush.setPosition(1);
+    }
+
+    public void resetPushIntake() {
+        servoPush.setPosition(0);
+    }
+}
+\
